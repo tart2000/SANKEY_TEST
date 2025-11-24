3b979e67f6ed9facd83049149029d8b137c1e7c4
Arthur <tart2000design@gmail.com>
2025-11-24 12:33:26 +0100

    works when isEditable : false

--- src/app/lot/layout.tsx
+++ src/app/lot/layout.tsx
@@ -1,8 +1,9 @@
 export default function LotLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr" style={{ margin: 0, padding: 0, height: '100%' }}>
-      <head>
-        <style>{`
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
           html, body {
             margin: 0 !important;
             padding: 0 !important;
@@ -14,11 +15,10 @@ export default function LotLayout({ children }: { children: React.ReactNode }) {
             padding: 0 !important;
             height: 100%;
           }
-        `}</style>
-      </head>
-      <body style={{ margin: 0, padding: 0, height: '100%', overflow: 'auto' }}>
-        {children}
-      </body>
-    </html>
+        `,
+        }}
+      />
+      {children}
+    </>
   );
 }
